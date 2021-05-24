560897c5fdd0675671c093bca073ef67c4e397fc
QiongwenXu <qwxu@outlook.com>
2021-05-24 00:37:19 -0500

    modify parameter xdp1_kern to xdp1_kern.o in object file experiment

--- 2_different_inputs/3_object_file/k2.sh
+++ 2_different_inputs/3_object_file/k2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running K2 to optimize benchmark..."
-benchmark=xdp1_kern
+benchmark=xdp1_kern.o
 section=xdp1
 if [ $# -gt 0 ]; then
   benchmark=$1
@@ -9,11 +9,13 @@ if [ $# -gt 1 ]; then
   section=$2
 fi
 echo "Benchmark is" $benchmark $section
-output_file=$benchmark.o.out
+output_file=$benchmark.out
 if [ ! -d "output" ]; then
   mkdir output
 fi
-python3 run_k2.py $benchmark.o $benchmark.desc $benchmark.k2_args --programs $section > run_k2_log.tmp
+
+bm_name=${benchmark%??}
+python3 run_k2.py $bm_name.o $bm_name.desc $bm_name.k2_args --programs $section > run_k2_log.tmp
 mv output/${section}_modified.o $output_file
 echo "Finish running:"
 grep "original" output/log.txt
@@ -22,7 +24,7 @@ grep "top 1 " output/log.txt
 echo "Optimized program is stored in" $output_file
 grep "compiling" output/log.txt
 rm -rf output
-rm -rf benchmark.insns
+rm -rf $bm_name.insns
 rm -rf run_k2_log.tmp
 rm -rf old-extracted-files
 
